6350aa55c266e35781fc7412b63ecea773d00d2d
Thiago Lanza <lanzathm@gmail.com>
2022-06-14 16:57:45 -0300

    Initial commit: project scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SearchAddress />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchAddress from './components/SearchAddress.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchAddress
   }
 }
 </script>
---
+++ src/components/SearchAddress.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+		<input
+			class="main-input"
+			v-model="cep"
+			type="number"
+			name="cep"
+			placeholder="Digite um cep para buscar..."
+		>
+		<h1>{{ cep }}</h1>
+  </div>
+</template>
+
+<script>
+/*eslint no-unused-vars: "warn"*/
+import axios from 'axios'
+
+export default {
+  name: 'SearchAddress',
+	data() {
+		return {
+			cep: null,
+			addresses: null
+		}
+	},
+	methods: {  },
+	async created() {
+		this.addresses = await axios
+				.get('http://localhost:8000/api/v1/address/')
+				.then(res => res.data)
+				.catch(err => console.log(err.message))
+
+		console.log(this.addresses)
+	}
+}
+</script>
+
+<style scoped>
+  .main-input {
+		width: 200px;
+		height: 10px;
+		border-radius: 12px;
+		padding: 10px;
+	}
+	h3 {
+		margin: 40px 0 0;
+	}
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+	li {
+		display: inline-block;
+		margin: 0 10px;
+	}
+	a {
+		color: #42b983;
+	}
+</style>
